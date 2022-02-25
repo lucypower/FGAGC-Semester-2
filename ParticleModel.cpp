c28ea65e9ed617c481c2ba0b851dd319ab45da23
lucypower <luciellepenguin9@gmail.com>
2022-02-25 11:04:20 +0000

    1

--- ParticleModel.cpp
+++ ParticleModel.cpp
@@ -9,3 +9,18 @@ ParticleModel::~ParticleModel()
 {
 
 }
+
+void ParticleModel::Update()
+{
+	moveConstVelocity();
+}
+
+void ParticleModel::moveConstVelocity(const float deltaTime)
+{
+	_position = _position + (_velocity * deltaTime);
+}
+
+void ParticleModel::moveConstAcceleration(const float deltaTime)
+{
+	_position = _position + _velocity * deltaTime + 0.5 * _acceleration * time * time;
+}
--- ParticleModel.h
+++ ParticleModel.h
@@ -13,8 +13,17 @@ public :
 	ParticleModel();
 	~ParticleModel();
 
+	void Update();
+
+	void moveConstVelocity(const float deltaTime);
+	void moveConstAcceleration(const float deltaTime);
+
 private :
 
 	XMFLOAT4X4 _world;
+	
+	XMFLOAT3 _velocity;
+	XMFLOAT3 _position;
+	XMFLOAT3 _acceleration;
 };
 
