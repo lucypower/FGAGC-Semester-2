e19750e935357012a92c7f1d02b72b7efa92210b
lucypower <luciellepenguin9@gmail.com>
2022-04-01 14:55:50 +0100

    friday before lesson

--- ParticleSystem.cpp
+++ ParticleSystem.cpp
@@ -1,1 +1,23 @@
 #include "ParticleSystem.h"
+
+ParticleSystem::ParticleSystem(Transform* transform, ParticleModel* particleModel, Appearance* appearance, float mass) : _mass(mass)
+{
+	_position = transform->GetPosition();
+	_velocity = particleModel->GetVelocity();
+	//_colour = appearance->GetMaterial();
+}
+
+ParticleSystem::~ParticleSystem()
+{
+
+}
+
+void ParticleSystem::Update(const float deltaTime)
+{
+
+}
+
+void ParticleSystem::Draw()
+{
+
+}
--- ParticleSystem.h
+++ ParticleSystem.h
@@ -1,15 +1,29 @@
 #pragma once
+#include <vector>
+#include "Vector3D.h"
+#include "Transform.h"
+#include "Appearance.h"
+#include "ParticleModel.h"
+
 class ParticleSystem
 {
-	int NumParticles;
-
 public :
 
-	ParticleSystem();
+	ParticleSystem(Transform* transform, ParticleModel* particleModel, Appearance* appearance, float mass);
 	~ParticleSystem();
 
 	void Update(const float deltaTime);
 	void Draw();
 
+private :
+
+	int _numParticles;
+	float _mass;
+
+	Vector3D _position;
+	Vector3D _colour;
+	Vector3D _velocity;
+
+	std::vector<ParticleSystem> particles;
 };
 
